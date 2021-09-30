5474257aca0662c9ee0511ae4e7bf23f79151faa
limgeonho <ghlim909@gmail.com>
2021-10-01 01:40:21 +0900

    2021.10.01

--- 그래프알고리즘/16929_towdots.py
+++ 그래프알고리즘/16929_towdots.py
@@ -4,55 +4,35 @@ dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
 
-def check(x, y, color):
-    global start_x, start_y, flag
+def go(x, y, px, py, color):
+    if visited[x][y]:
+        return True
     visited[x][y] = True
-    visited[start_x][start_y] = False
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0<=nx<n and 0<=ny<m and not visited[nx][ny] and array[nx][ny] == color:
-            if nx == start_x and ny == start_y:
-                flag = True
-                return
-            visited[nx][ny] = True
-            check(nx, ny, color)
-            continue
+    for k in range(4):
+        nx = x + dx[k]
+        ny = y + dy[k]
+        if 0<=nx<n and 0<=ny<m:
+            if (nx, ny) == (px, py):
+                continue
+            if array[nx][ny] == color:
+                if go(nx, ny, x, y, color):
+                    return True
     return False
-
-def find_start(array, color):
-    for i in range(n):
-        for j in range(m):
-            if array[i][j] == color:
-                return (i, j)
-
 
 n, m = map(int, input().split())
 
-color = set()
-
 # 입력
-array = []
-for _ in range(n):
-    array.append(input())
+array = [input() for _ in range(n)]
+visited = [[False] * m for _ in range(n)]
 
-# 색깔추출
 for i in range(n):
     for j in range(m):
-        color.add(array[i][j])
+        if visited[i][j]:
+            continue
+        flag = go(i, j, -1, -1, array[i][j])
+        if flag:
+            print('Yes')
+            exit()
 
-
-for _ in range(len(color)):
-    c = color.pop()
-    start_x, start_y = find_start(array, c)
-    visited = [[False] * m for _ in range(n)]
-    flag = False
-    check(start_x, start_y, c)
-    if flag:
-        break
-
-if flag:
-    print('YES')
-else:
-    print('NO')
+print('No')
 
